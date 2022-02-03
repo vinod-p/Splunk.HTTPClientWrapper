1c0a823fbc5297145190cf1b1b762a2db328cfac
vinod-p <41475024+vinod-p@users.noreply.github.com>
2022-02-03 16:35:07 +0530

    code added
    Code added

---
+++ Splunk.HTTPClientWrapper/HTTPClientWrapper.cs
@@ -0,0 +1,115 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Splunk.HTTPClientWrapper
+{
+    /// <summary>
+    /// A generic wrapper class to REST API calls
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static class HTTPClientWrapper<T> where T : class
+    {
+        /// <summary>
+        /// For getting the resources from a web api
+        /// </summary>
+        /// <param name="url">API Url</param>
+        /// <returns>A Task with result object of type T</returns>
+        public static async Task<T> Get(string url)
+        {
+            T result = null;
+            using (var httpClient = new HttpClient())
+            {
+                var response = httpClient.GetAsync(new Uri(url)).Result;
+                response.EnsureSuccessStatusCode();
+                await response.Content.ReadAsStringAsync().ContinueWith((Task<string> x) =>
+                {
+                    if (x.IsFaulted)
+                        throw x.Exception;
+
+                    result = JsonConvert.DeserializeObject<T>(x.Result);
+                });
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// For creating a new item over a web api using POST
+        /// </summary>
+        /// <param name="apiUrl">API Url</param>
+        /// <param name="postObject">The object to be created</param>
+        /// <returns>A Task with created item</returns>
+        public static async Task<T> PostRequest(string apiUrl, T postObject)
+        {
+            T result = null;
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync(apiUrl, postObject, new JsonMediaTypeFormatter()).ConfigureAwait(false);
+
+                response.EnsureSuccessStatusCode();
+
+                await response.Content.ReadAsStringAsync().ContinueWith((Task<string> x) =>
+                {
+                    if (x.IsFaulted)
+                        throw x.Exception;
+
+                    result = JsonConvert.DeserializeObject<T>(x.Result);
+
+                });
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// For updating an existing item over a web api using PUT
+        /// </summary>
+        /// <param name="apiUrl">API Url</param>
+        /// <param name="putObject">The object to be edited</param>
+        public static async Task PutRequest(string apiUrl, T putObject)
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.PutAsync(apiUrl, putObject, new JsonMediaTypeFormatter()).ConfigureAwait(false);
+
+                response.EnsureSuccessStatusCode();
+            }
+        }
+        /// <summary>
+        /// Gets the token.
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<string> GetToken()
+        {
+            using HttpClient client = new();
+
+            client.DefaultRequestHeaders.Add("", "");
+
+            var credentials = new object();
+
+            var serializedCredentialsObj = JsonConvert.SerializeObject(credentials);
+
+            var content = new StringContent(serializedCredentialsObj, Encoding.UTF8, "application/json");
+
+            var result = await client.PostAsync("", content);
+
+            result.EnsureSuccessStatusCode();
+
+            if (result.IsSuccessStatusCode && result.Content != null)
+            {
+
+                var data = await result.Content.ReadAsStringAsync();
+                // Deserialize the data object to ready token
+                return "";// retun token here
+            }
+
+            return null;// default return value is NULL
+        }
+
+    }
+}
